d6e14314a7f21f9612970038223ddf8e8c524cbb
RahienL <r.loncopan@duocuc.cl>
2026-05-05 16:26:31 -0400

    prueba de inventario

---
+++ service-inventario/src/main/java/com/musa_jeans/service_inventario/repository/InventarioRepository.java
@@ -0,0 +1,9 @@
+package com.musa_jeans.service_inventario.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.musa_jeans.service_inventario.model.Inventario;
+
+public interface InventarioRepository extends JpaRepository<Inventario, Long>{
+
+}
--- service-jean/src/main/java/com/musa_jeans/service_jean/repository/MarcaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.musa_jeans.service_jean.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.musa_jeans.service_jean.model.Marca;
-
-public interface MarcaRepository extends JpaRepository<Marca, Long> {
-
-}
