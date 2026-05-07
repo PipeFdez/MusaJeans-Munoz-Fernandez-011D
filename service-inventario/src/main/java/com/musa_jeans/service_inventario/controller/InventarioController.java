87a8b1159527c97fe472c7f9aa832392eb48079b
RahienL <r.loncopan@duocuc.cl>
2026-05-07 15:13:12 -0400

    ingreso de actualizar stock en service y controller

--- service-inventario/src/main/java/com/musa_jeans/service_inventario/controller/InventarioController.java
+++ service-inventario/src/main/java/com/musa_jeans/service_inventario/controller/InventarioController.java
@@ -3,8 +3,11 @@ package com.musa_jeans.service_inventario.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,5 +31,13 @@ public class InventarioController {
 
         return inventarioService.obtenerInventarioCompleto(id);
         
+    }
+
+    @PutMapping("/actualizar/{id}")
+    public ResponseEntity<Inventario> actualizar(@PathVariable Long id, @RequestBody Inventario datosNuevos) {
+
+        Inventario actualizado = inventarioService.actualizarInventario(id, datosNuevos);
+        
+        return ResponseEntity.ok(actualizado);
     }
 }
--- service-inventario/src/main/java/com/musa_jeans/service_inventario/service/InventarioService.java
+++ service-inventario/src/main/java/com/musa_jeans/service_inventario/service/InventarioService.java
@@ -3,7 +3,11 @@ package com.musa_jeans.service_inventario.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import com.musa_jeans.service_inventario.model.Inventario;
@@ -49,6 +53,14 @@ public class InventarioService {
         }
         return inventario;
     }
+
+    public Inventario actualizarInventario(Long id, Inventario datosNuevos) {
+        return inventarioRepository.findById(id).map(inventario -> {
+            inventario.setStock(datosNuevos.getStock());
+            return inventarioRepository.save(inventario);
+        }).orElseThrow(() -> new RuntimeException("Inventario no encontrado con ID: " + id));
+    }
+
 }
 
 
